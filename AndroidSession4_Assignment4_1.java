c1d845cfe20591f1cdbebbedf9cd3ca49b2b070e
prapul <prapulpoonacha@gmail.com>
2016-12-26 17:01:05 +0530

    constructors

---
+++ AndroidSession4_Assignment4_1.java
@@ -0,0 +1,13 @@
+package prapul;
+
+public class AndroidSession4_Assignment4_1 {
+	public void print(){
+		System.out.println("null");
+	}
+	public void print(int z){
+		System.out.println(z);
+	}
+	public void print(int x, int y) {
+		System.out.println(x + "," + y);
+	}
+}
---
+++ AndroidSession4_Assignment4_2.java
@@ -0,0 +1,10 @@
+package prapul;
+
+public class AndroidSession4_Assignment4_2{
+	public static void main(String[] args) {
+		AndroidSession4_Assignment4_1 obj = new AndroidSession4_Assignment4_1();
+		obj.print();
+		obj.print(2);
+		obj.print(2, 4);
+	}
+}
